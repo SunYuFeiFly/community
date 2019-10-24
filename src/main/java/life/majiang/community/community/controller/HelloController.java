8f64b4c9570b88ae73651b9d9139aaeec2464611
SunYuFeiFly <1049924914@qq.com>
2019-10-24 15:52:54 +0800

    init repo

---
+++ src/main/java/life/majiang/community/community/controller/HelloController.java
@@ -0,0 +1,19 @@
+package life.majiang.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.jws.WebParam;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+
+}
